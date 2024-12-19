24f608e5fa42cd606fb49a9e2b1ef7b3055ca336
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-12-18 23:09:56 -0800

    feat: services page

--- src/lib/stores.svelte.ts
+++ src/lib/stores.svelte.ts
@@ -12,6 +12,7 @@ export type Service = typeof services[number];
 
 type ServiceData = {
 	[key: Service]: {
+		caption: string;
 		applications: string[];
 		description: string;
 		colorChartPoints: string[];
@@ -20,10 +21,11 @@ type ServiceData = {
 
 export const serviceData: ServiceData = {
 	'metalic-epoxy-floors': {
+		caption: "Vibrant Colors",
 		applications: [
+			"House Basement",
 			"Car Dealerships",
 			"Garages",
-			"House Basement",
 			"Residential",
 			"Shopping Malls",
 			"Showrooms",
@@ -36,15 +38,16 @@ export const serviceData: ServiceData = {
 		]
 	},
 	'flake-epoxy-floors': {
+		caption: "Wide choice of blends",
 		applications: [
+			"House Basement",
 			"Patios",
 			"Garages",
-			"House Basement",
 			"Schools",
 			"Grocery Stores",
 			"Showrooms",
 		],
-		description: "Elevate your interiors with Luxury Floors' flake epoxy flooring, the perfect blend of durability, style, and functionality for homes and businesses in Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland. Flake epoxy flooring is a versatile and eye-catching solution, combining multi-colored flakes with durable epoxy coatings to create a textured, seamless finish that is both slip-resistant and visually appealing.<br>Ideal for garages, basements, commercial spaces, and industrial settings, flake epoxy floors offer superior resistance to wear, chemicals, and heavy foot or vehicle traffic. The textured surface not only enhances safety but also provides a modern aesthetic that complements any space. Popular in automotive shops, gyms, warehouses, and residential spaces, flake epoxy floors are the go-to choice for those seeking long-lasting performance with a touch of personality.<br>Our expert team handles every step of the process with precision and care—from surface preparation and application of primers to the layering of decorative flakes in your chosen color palette. A clear topcoat is then applied for added durability, ensuring a smooth, low-maintenance surface that retains its beauty for years to come.<br>Choose Luxury Floors for premium flake epoxy flooring services and transform your space into a durable, stylish, and functional masterpiece. Trust our certified professionals to deliver exceptional quality that exceeds your expectations.<br>",
+		description: "Elevate your interiors with Luxury Floors' flake epoxy flooring, the perfect blend of durability, style, and functionality for homes and businesses in Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland. Flake epoxy flooring is a versatile and eye-catching solution, combining multi-colored flakes with durable epoxy coatings to create a textured, seamless finish that is both slip-resistant and visually appealing.<br><br>Ideal for garages, basements, commercial spaces, and industrial settings, flake epoxy floors offer superior resistance to wear, chemicals, and heavy foot or vehicle traffic. The textured surface not only enhances safety but also provides a modern aesthetic that complements any space. Popular in automotive shops, gyms, warehouses, and residential spaces, flake epoxy floors are the go-to choice for those seeking long-lasting performance with a touch of personality.<br>Our expert team handles every step of the process with precision and care—from surface preparation and application of primers to the layering of decorative flakes in your chosen color palette. A clear topcoat is then applied for added durability, ensuring a smooth, low-maintenance surface that retains its beauty for years to come.<br><br>Choose Luxury Floors for premium flake epoxy flooring services and transform your space into a durable, stylish, and functional masterpiece. Trust our certified professionals to deliver exceptional quality that exceeds your expectations.<br><br>",
 		colorChartPoints: [
 			"High-Quality Finishes",
 			"Versatile Looks",
@@ -52,15 +55,16 @@ export const serviceData: ServiceData = {
 		]
 	},
 	'epoxy-countertops': {
+		caption: "Seemless and hygienic",
 		applications: [
-			"Residential Kitchens",
-			"Dining and Coffee Table",
-			"Bathrooms",
-			"Shopping Malls",
 			"Bar Top",
+			"Residential Kitchens",
+			"Bathrooms",
+			"Dining and Coffee Table",
+			"Shopping Malls",
 			"Retail and commercial Shops",
 		],
-		description: "Transform your countertops into stunning, high-end surfaces with Luxury Floors' premium epoxy countertop solutions. Serving Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland, we specialize in creating custom epoxy countertops that combine durability, functionality, and unmatched aesthetics. Whether you desire the look of marble, quartz, or unique artistic patterns, our advanced epoxy coatings deliver a luxurious, seamless finish that enhances any space.<br> Perfect for residential and commercial settings, epoxy countertops are a popular choice for kitchens, bathrooms, bar tops, and workspaces. With their high-gloss finish, non-porous surface, and exceptional resistance to scratches, stains, and heat, they provide both beauty and practicality. From elegant kitchens to statement-making office desks, epoxy countertops are the ultimate blend of form and function.<br> Our experts take pride in precision and artistry at every stage—from surface preparation to the meticulous application of colors, patterns, and protective topcoats. Each countertop is custom-crafted to reflect your vision and style, ensuring a truly personalized result. Our UV-resistant and high-wear finishes ensure that your epoxy countertop remains vibrant and resilient for years to come.<br> At Luxury Floors, we’re committed to delivering exceptional results that redefine interior spaces. Let our skilled team create epoxy countertops that not only serve as functional surfaces but also as conversation-worthy works of art. Choose Luxury Floors for the best in epoxy countertop solutions and experience the pinnacle of luxury and craftsmanship.",
+		description: "Transform your countertops into stunning, high-end surfaces with Luxury Floors' premium epoxy countertop solutions. Serving Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland, we specialize in creating custom epoxy countertops that combine durability, functionality, and unmatched aesthetics. Whether you desire the look of marble, quartz, or unique artistic patterns, our advanced epoxy coatings deliver a luxurious, seamless finish that enhances any space.<br><br> Perfect for residential and commercial settings, epoxy countertops are a popular choice for kitchens, bathrooms, bar tops, and workspaces. With their high-gloss finish, non-porous surface, and exceptional resistance to scratches, stains, and heat, they provide both beauty and practicality. From elegant kitchens to statement-making office desks, epoxy countertops are the ultimate blend of form and function.<br><br> Our experts take pride in precision and artistry at every stage—from surface preparation to the meticulous application of colors, patterns, and protective topcoats. Each countertop is custom-crafted to reflect your vision and style, ensuring a truly personalized result. Our UV-resistant and high-wear finishes ensure that your epoxy countertop remains vibrant and resilient for years to come.<br><br> At Luxury Floors, we’re committed to delivering exceptional results that redefine interior spaces. Let our skilled team create epoxy countertops that not only serve as functional surfaces but also as conversation-worthy works of art. Choose Luxury Floors for the best in epoxy countertop solutions and experience the pinnacle of luxury and craftsmanship.",
 		colorChartPoints: [
 			"High-Quality Finishes",
 			"Versatile Looks",
@@ -68,30 +72,32 @@ export const serviceData: ServiceData = {
 		]
 	},
 	'texture-deck': {
+		caption: "Durable surface",
 		applications: [
-			"Pool Decks",
-			"Stairs and Ramps",
-			"Balconies and Rooftops",
-			"Commercial Areas",
 			"Walkways and Driveways",
+			"Pool Decks",
+			"Balconies and Rooftops",
+			"Stairs and Ramps",
+			"Commercial Areas",
 			"Sports Complexes",
 		],
-		description: "Transform your decks, patios, and outdoor spaces with Luxury Floors' premium texture deck solutions. Serving Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland, we specialize in high-quality textured coatings that blend durability, safety, and stunning design. Whether you want a sleek modern look or a decorative custom finish, our advanced texture deck systems provide a stylish, slip-resistant surface that enhances both aesthetics and functionality.<br> Perfect for residential and commercial applications, texture deck coatings are ideal for pool decks, balconies, walkways, and driveways. With their weatherproof and UV-resistant properties, these coatings stand up to the elements while maintaining their vibrant appearance. Designed to withstand wear and tear, they ensure long-lasting performance in high-traffic areas.<br> Our experts excel in precision and craftsmanship, handling every detail—from surface preparation to the application of durable coatings in patterns and colors tailored to your vision. Each deck is finished with advanced sealing technology, ensuring your surface remains easy to maintain and resistant to moisture, cracking, and fading.<br> At Luxury Floors, we’re passionate about creating spaces that are as functional as they are beautiful. Our texture deck coatings not only add safety and style to your outdoor areas but also increase the value of your property. Choose Luxury Floors for premium texture deck solutions and experience the perfect blend of luxury, durability, and design expertise. Let us bring your outdoor vision to life with our commitment to excellence and innovation.",
+		description: "Transform your decks, patios, and outdoor spaces with Luxury Floors' premium texture deck solutions. Serving Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland, we specialize in high-quality textured coatings that blend durability, safety, and stunning design. Whether you want a sleek modern look or a decorative custom finish, our advanced texture deck systems provide a stylish, slip-resistant surface that enhances both aesthetics and functionality.<br><br> Perfect for residential and commercial applications, texture deck coatings are ideal for pool decks, balconies, walkways, and driveways. With their weatherproof and UV-resistant properties, these coatings stand up to the elements while maintaining their vibrant appearance. Designed to withstand wear and tear, they ensure long-lasting performance in high-traffic areas.<br><br> Our experts excel in precision and craftsmanship, handling every detail—from surface preparation to the application of durable coatings in patterns and colors tailored to your vision. Each deck is finished with advanced sealing technology, ensuring your surface remains easy to maintain and resistant to moisture, cracking, and fading.<br><br> At Luxury Floors, we’re passionate about creating spaces that are as functional as they are beautiful. Our texture deck coatings not only add safety and style to your outdoor areas but also increase the value of your property. Choose Luxury Floors for premium texture deck solutions and experience the perfect blend of luxury, durability, and design expertise. Let us bring your outdoor vision to life with our commitment to excellence and innovation.",
 		colorChartPoints: [
 			"High-Quality Finishes",
 			"Versatile Looks",
 		]
 	},
 	'concrete-walls': {
+		caption: "Durable and weather-resistant",
 		applications: [
+			"Walkways and Driveways",
+			"Parking Lots",
 			"Residential",
 			"Playgrounds",
-			"Parking Lots",
-			"Commercial Areas",
-			"Walkways and Driveways",
 			"Airports",
+			"Commercial Areas",
 		],
-		description: "At Luxury Floors, we recognize the importance of maintaining the integrity and visual appeal of your asphalt surfaces. Serving Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland, we specialize in expert asphalt repair solutions designed to extend the lifespan of your surfaces while enhancing their functionality and aesthetics. Whether it's minor surface damage or significant wear, our services cater to a variety of residential, commercial, and industrial needs.<br> Ideal for driveways, parking lots, roads, walkways, and industrial yards, our asphalt repair solutions address common issues such as cracks, potholes, rutting, and surface degradation. Using high-quality materials and advanced techniques, we ensure that repaired surfaces are durable, weather-resistant, and capable of handling heavy traffic and constant wear.<br> Our skilled team of professionals begins each project with a thorough surface evaluation to determine the root cause of damage. We utilize cutting-edge repair methods—such as crack sealing, pothole patching, resurfacing, and full-depth reclamation—to restore your asphalt to its original strength and smoothness. Additionally, we offer sealing services that provide enhanced protection against moisture, UV damage, and the elements, prolonging the lifespan of your asphalt surfaces.<br> At Luxury Floors, we take pride in delivering high-quality, long-lasting results that combine durability, safety, and aesthetics. Whether you're restoring a residential driveway, upgrading a commercial parking lot, or maintaining an industrial facility, our asphalt repair services are tailored to meet your specific needs. Choose Luxury Floors for reliable and innovative asphalt repair solutions, and let us help you restore the strength and beauty of your surfaces with unmatched expertise and attention to detail.",
+		description: "At Luxury Floors, we recognize the importance of maintaining the integrity and visual appeal of your asphalt surfaces. Serving Vancouver, Richmond, Burnaby, Surrey, and the Lower Mainland, we specialize in expert asphalt repair solutions designed to extend the lifespan of your surfaces while enhancing their functionality and aesthetics. Whether it's minor surface damage or significant wear, our services cater to a variety of residential, commercial, and industrial needs.<br><br> Ideal for driveways, parking lots, roads, walkways, and industrial yards, our asphalt repair solutions address common issues such as cracks, potholes, rutting, and surface degradation. Using high-quality materials and advanced techniques, we ensure that repaired surfaces are durable, weather-resistant, and capable of handling heavy traffic and constant wear.<br><br> Our skilled team of professionals begins each project with a thorough surface evaluation to determine the root cause of damage. We utilize cutting-edge repair methods—such as crack sealing, pothole patching, resurfacing, and full-depth reclamation—to restore your asphalt to its original strength and smoothness. Additionally, we offer sealing services that provide enhanced protection against moisture, UV damage, and the elements, prolonging the lifespan of your asphalt surfaces.<br><br> At Luxury Floors, we take pride in delivering high-quality, long-lasting results that combine durability, safety, and aesthetics. Whether you're restoring a residential driveway, upgrading a commercial parking lot, or maintaining an industrial facility, our asphalt repair services are tailored to meet your specific needs. Choose Luxury Floors for reliable and innovative asphalt repair solutions, and let us help you restore the strength and beauty of your surfaces with unmatched expertise and attention to detail.",
 		colorChartPoints: [
 			"High-Quality Finishes",
 			"Versatile Looks",
--- src/routes/services/[id]/+page.svelte
+++ src/routes/services/[id]/+page.svelte
@@ -1,10 +1,56 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { serviceData } from '$lib/stores.svelte';
+	import * as Carousel from '$lib/components/ui/carousel';
 
+	let selectedServiceData = $derived(serviceData[$page.params.id] || {});
+	$inspect(selectedServiceData);
 
-
+	$effect(() => {
+		if (!selectedServiceData) {
+			goto('/services');
+		}
+	});
 </script>
 
+<main class="mt-16 *:p-32">
+	<!-- Carousel -->
+	<section class="flex flex-col gap-20 bg-black text-center text-4xl text-white">
+		<h1 class="font-[Cantarell]">
+			Why {$page.params.id
+				.split('-')
+				.map((x) => x[0].toUpperCase() + x.slice(1))
+				.join(' ')}s?
+		</h1>
 
+		<!-- TODO: create carousel -->
 
+		<h2 class="font-[Alatsi] text-2xl font-semibold">{selectedServiceData.caption}</h2>
+	</section>
 
+	<!-- Applications -->
+	<section class="font-[Cantarell]">
+		<h3 class="mb-16 text-center text-2xl font-semibold">Applications</h3>
+		<div class="grid w-full grid-cols-3 grid-rows-2">
+			{#each selectedServiceData.applications as application, idx}
+				<figure class="flex aspect-square flex-col items-center justify-center gap-4">
+					<img src="/{$page.params.id}/applications/{idx}.png" alt="" class="w-[15vw]" />
+					<figcaption class="text-sm">{application}</figcaption>
+				</figure>
+			{/each}
+		</div>
+	</section>
 
+	<!-- Description -->
+	<section class="bg-black font-[Cantarell] leading-8 text-white">
+		{@html selectedServiceData.description}
+	</section>
+
+	<!-- Materials -->
+	<section>
+		<img src="/{$page.params.id}/pallet/mats.png" alt="" class="mb-4" />
+
+		<div class=""></div>
+	</section>
+</main>
