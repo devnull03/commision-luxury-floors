7f89b048aa4559276ed2eb9ba49a195fe839a793
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-03-03 12:53:48 -0800

    fix: vercel scripts location update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
 	import '../app.css';
-	import { onMount } from 'svelte';
-	import { fade, fly } from 'svelte/transition';
-	import { navigating } from '$app/stores';
+	import { fade } from 'svelte/transition';
 	import { PUBLIC_COMPANY_NAME } from '$env/static/public';
-	import { dev } from '$app/environment';
-
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-	import { Angle, ArrowUp, CaretUp } from 'svelte-radix';
+	import { CaretUp } from 'svelte-radix';
 
 	interface Props {
 		children?: import('svelte').Snippet;
@@ -19,21 +13,10 @@
 
 	let scrollY = $state(0);
 	let { children }: Props = $props();
-
-	// let firstLoad = $state(true);
-	// let load = $derived(firstLoad || !$navigating);
-
-	injectAnalytics({ mode: dev ? 'development' : 'production' });
-	injectSpeedInsights();
-
-	// onMount(() => {
-	// 	firstLoad = false;
-	// });
 </script>
 
 <svelte:head>
 	<title>{PUBLIC_COMPANY_NAME}</title>
-	<!-- <script src="https://kit.fontawesome.com/30f055fc02.js" crossorigin="anonymous"></script> -->
 
 	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
 	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
